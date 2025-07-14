7fa38fb2ee8583f26d9fd4968f74f8639c8a80f7
Tuğrul Severoğlu <hammeyva@Tugruls-MacBook-Pro.local>
2025-07-14 22:06:37 +0300

    a

--- public/account-config.js
+++ public/account-config.js
@@ -1,5 +1,5 @@
 // Uygulamanın kullanacağı Interactive Brokers hesap konfigürasyonu
 window.IB_CONFIG = {
-  ACCOUNT_ID: 'U7960949',  // Kullanılacak hesap ID'si
+  ACCOUNT_ID: 'U7960949',  // Kullanılacak hesap ID'si - Updated to match IBGateway login
   API_BASE_URL: '/api/proxy'
 }; 
